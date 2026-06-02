4f3277644fe6cabb93bec39187107026e6e08eca
duanbaihu <duanbohu@zmn.cn>
2026-06-02 12:40:23 +0800

    优化 OpenLDAP 配置并支持从节点 RID 自定义
    - 增加 LDAP_SLAVE_RID 环境变量以支持从节点标识自定义
    - 更新 Schema 添加 isDefaultGroup 属性并调整对象类必须项
    - 优化 slapd 配置模块加载及 overlay 顺序，调整 syncprov 参数
    - 修正从节点 syncrepl 配置，移除冗余日志过滤并关闭 schema 检查

--- openldap/entrypoint.sh
+++ openldap/entrypoint.sh
@@ -13,6 +13,7 @@
 # LDAP_ADMIN_USER=admin 管理用户名 例：Manager
 # LDAP_ADMIN_PASSWORD=duanbai 管理用户的密码
 # LDAP_SYNC_USER=syncuser 同步用户
+# LDAP_SLAVE_RID=001 同步用户
 # LDAP_SYNC_USERPASS=syncuser123 同步用户的密码
 # LDAP_SYNC_SSL=True 同步是否启用SSL
 # LDAP_SYNC_PORT=389 主从同步的MASTER 端口
@@ -30,7 +31,7 @@ if [ ! -d "${LDAP_CONF_DIR}/slapd.d/cn=config" ]; then
   help
   init_config
 fi
-formatOutput "title" 
+formatOutput "title"
 # 正试启动应用
 formatOutput "Starting \033[32mSlapd\033[0m Service...."
 printOK $?
